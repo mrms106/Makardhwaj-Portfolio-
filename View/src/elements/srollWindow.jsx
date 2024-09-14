a6743d8cdf432c779684f99d094e839160cdcae3
Makardhwaj <mhs93679@gmail.com>
2024-09-14 17:37:05 +0530

    fixed scrolling issue

--- View/src/elements/srollWindow.jsx
+++ View/src/elements/srollWindow.jsx
@@ -4,7 +4,10 @@ import { useLocation } from 'react-router-dom';
 function ScrollWindow() {
   const { pathname } = useLocation();
   const handleScroll = () => {
-    if (window.innerWidth < 800) {
+    if (pathname === '/') {
+      window.scrollTo(0, 0); 
+    }else
+     if (window.innerWidth < 800) {
       window.scrollTo(0, 1200);
     } else {
       window.scrollTo(0, 0);
