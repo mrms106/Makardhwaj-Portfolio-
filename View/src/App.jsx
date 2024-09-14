44ff4c8f22cbe1dd10fd073aa101f4ad7a3f5cbd
Makardhwaj <mhs93679@gmail.com>
2024-09-14 23:53:18 +0530

    added pwa app

--- View/src/App.jsx
+++ View/src/App.jsx
@@ -5,21 +5,36 @@ import NavBar from './elements/include/navBar'
 import About from './elements/About/about'
 import CvMain from './elements/CVmain/cvMain';
 import Projects from './elements/project/project';
-import ContactMe from './elements/contact/contactMe';
+// import ContactMe from './elements/contact/contactMe';
+import ContactMe from './contactform'
 import DarkModeBtn from './elements/darkModebtn';
 import './style/darkmode.css'
 import ScrollWindow from './elements/srollWindow';
+import PwaInstall from './pwaApp';
+import darkbg from '../src/assets/darkbg.jpg'
 function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+
  const[darkMode,setdarkMode]=useState(()=>{
   const setDarkMode=localStorage.getItem('darkMode')
   return setDarkMode==='true'
  })
+
+ const handleBeforeInstallPrompt = (e) => {
+  e.preventDefault(); 
+  setDeferredPrompt(e); 
+};
  useEffect(()=>{
   localStorage.setItem('darkMode',darkMode)
+  window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+  return () => {
+      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+  }
  },[darkMode])
   return (
-   
+    
     <div className={darkMode ? "darkmode" : "body"}>
+       <PwaInstall deferredPrompt={deferredPrompt} setDeferredPrompt={setDeferredPrompt}/>
      <DarkModeBtn darkMode={darkMode} setdarkMode={setdarkMode}/>
     <div className='mainbox'>
       <Router>
--- View/src/main.jsx
+++ View/src/main.jsx
@@ -7,3 +7,19 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    const swUrl = `${import.meta.env.VITE_PUBLIC_URL || ''}/serviceWorker.js`;
+    if (swUrl) {
+      navigator.serviceWorker.register(swUrl)
+        .then((registration) => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch((error) => {
+          console.error('Service Worker registration failed:', error);
+        });
+    } else {
+      console.warn('Service Worker URL is not defined');
+    }
+  });
+}
